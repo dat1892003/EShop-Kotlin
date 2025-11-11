ea6db657aece6a136a3e15d7b2e6657e2a168279
dat1892003 <lethanhdat1892003@gmail.com>
2025-11-11 23:09:59 +0700

    Update data class

--- app/src/main/java/com/example/petshop/data/Rating.kt
+++ app/src/main/java/com/example/petshop/data/Rating.kt
@@ -8,5 +8,11 @@ data class Rating(
 )
 
 data class Json(
-
+    @SerializedName("id"          ) var id          : Int?    = null,
+    @SerializedName("title"       ) var title       : String? = null,
+    @SerializedName("price"       ) var price       : Double? = null,
+    @SerializedName("description" ) var description : String? = null,
+    @SerializedName("category"    ) var category    : String? = null,
+    @SerializedName("image"       ) var image       : String? = null,
+    @SerializedName("rating"      ) var rating      : Rating? = Rating()
 )
