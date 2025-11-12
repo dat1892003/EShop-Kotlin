3bab76f5184e2d1e304641ad991ed2aae409a170
dat1892003 <lethanhdat1892003@gmail.com>
2025-11-12 21:56:12 +0700

    Update data class

--- app/src/main/java/com/example/petshop/data/Product.kt
+++ app/src/main/java/com/example/petshop/data/Product.kt
@@ -1,5 +1,12 @@
 package com.example.petshop.data
+import com.google.gson.annotations.SerializedName
 
 data class Product(
-
+    @SerializedName("id"          ) var id          : Int?    = null,
+    @SerializedName("title"       ) var title       : String? = null,
+    @SerializedName("price"       ) var price       : Double? = null,
+    @SerializedName("description" ) var description : String? = null,
+    @SerializedName("category"    ) var category    : String? = null,
+    @SerializedName("image"       ) var image       : String? = null,
+    @SerializedName("rating"      ) var rating      : Rating? = Rating()
 )
